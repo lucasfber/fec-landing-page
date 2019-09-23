42da4f943a5e3f35e4a023022122e3de471531f7
Lucas Bertoldo <lucas.fber@gmail.com>
2019-09-23 18:17:26 -0300

    nextSlide working, animation not working yet

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@ $(document).ready(function() {
   const btnMenu = $("header .btn-menu");
   const btnClose = $(".overlay .btn-close");
   const navbar = $(".overlay .navbar");
+  const rightArrow = $(".right-arrow");
+  const slides = $(".last-news-slider .slide");
+  let slideIndex = 0;
 
   btnMenu.click(function() {
     navbar.toggleClass("visible");
@@ -10,4 +13,16 @@ $(document).ready(function() {
   btnClose.click(function() {
     navbar.toggleClass("visible");
   });
+
+  const nextSlide = function() {
+    slides.eq(slideIndex).toggleClass("active");
+    slideIndex++;
+    if (slideIndex >= slides.length) {
+      slideIndex = (1 % slides.length) - 1;
+      console.log("Entrou", slideIndex);
+    }
+    slides.eq(slideIndex).toggleClass("active");
+  };
+
+  rightArrow.click(nextSlide);
 });
