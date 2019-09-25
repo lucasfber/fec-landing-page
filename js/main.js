2bab5690e96e357a2792d4731b1f3ff6fb1f089a
Lucas Bertoldo <lucas.fber@gmail.com>
2019-09-25 11:02:56 -0300

    slider component done

--- js/main.js
+++ js/main.js
@@ -2,8 +2,9 @@ $(document).ready(function() {
   const btnMenu = $("header .btn-menu");
   const btnClose = $(".overlay .btn-close");
   const navbar = $(".overlay .navbar");
+  const leftArrow = $(".left-arrow");
   const rightArrow = $(".right-arrow");
-  const slides = $(".last-news-slider .slide");
+  const slides = $(".slider .slide");
   let slideIndex = 0;
 
   btnMenu.click(function() {
@@ -15,14 +16,36 @@ $(document).ready(function() {
   });
 
   const nextSlide = function() {
-    slides.eq(slideIndex).toggleClass("active");
-    slideIndex++;
+    if (slides.eq(slideIndex).hasClass("from-left")) {
+      slides.eq(slideIndex).toggleClass("active from-left");
+      slideIndex++;
+    } else {
+      slides.eq(slideIndex).toggleClass("active from-right");
+      slideIndex++;
+    }
+
     if (slideIndex >= slides.length) {
       slideIndex = (1 % slides.length) - 1;
-      console.log("Entrou", slideIndex);
     }
-    slides.eq(slideIndex).toggleClass("active");
+    slides.eq(slideIndex).toggleClass("active from-right");
+  };
+
+  const previousSlide = function() {
+    if (slides.eq(slideIndex).hasClass("from-right")) {
+      slides.eq(slideIndex).toggleClass("active from-right");
+      slideIndex--;
+    } else {
+      slides.eq(slideIndex).toggleClass("active from-left");
+      slideIndex--;
+    }
+
+    if (slideIndex < 0) {
+      slideIndex = slides.length - 1;
+    }
+
+    slides.eq(slideIndex).toggleClass("active from-left");
   };
 
   rightArrow.click(nextSlide);
+  leftArrow.click(previousSlide);
 });
